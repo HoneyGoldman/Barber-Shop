f163aaf5c04504c75046284315f7ce3922bf7c23
ShirT14 <47032929+ShirT14@users.noreply.github.com>
2022-07-15 13:02:18 +0300

    fixed login  & logOut logic

--- src/app/navigation-bar/navigation-bar.component.ts
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Admin } from 'src/Model/Admin';
 import { DataServiceComponent } from '../data-service/data-service.component';
 
 @Component({
@@ -8,7 +10,7 @@ import { DataServiceComponent } from '../data-service/data-service.component';
 })
 export class NavigationBarComponent implements OnInit {
   login: boolean = false;
-  constructor(private dataService: DataServiceComponent) { }
+  constructor(private dataService: DataServiceComponent, private router: Router) { }
 
   ngOnInit(): void {
     console.log("login is " + this.login)
@@ -17,9 +19,16 @@ export class NavigationBarComponent implements OnInit {
       if ('{}' !== JSON.stringify(admin)) {
         this.login = true; //debug false
       } else {
-        this.login = true; // debug false
+        this.login = false; // debug false
       }
     })
+  }
+
+  logOut() {
+    this.login=false;
+    this.dataService.storeAdmin(new Admin());
+    this.dataService.storeSiteId('');
+    this.router.navigateByUrl('');
   }
 
 }
