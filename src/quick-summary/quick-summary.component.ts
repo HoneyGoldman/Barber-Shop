6437281abe8d1b6e41f80ad09d4b545a521965c7
HoneyGoldman <honeygoldmanil@gmail.com>
2022-10-20 23:18:22 +0300

    added quick summary files

---
+++ src/quick-summary/quick-summary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quick-summary',
+  templateUrl: './quick-summary.component.html',
+  styleUrls: ['./quick-summary.component.css']
+})
+export class QuickSummaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
